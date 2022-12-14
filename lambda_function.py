c55f9cc66a83847710787a34713283d8c26e11d5
philjen35 <philjen35@yahoo.com>
2022-12-14 10:16:25 -0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event,context):
   return {
     'statusCode': 200,
-    'body': json.dumps("Hello world from github")
+    'body': json.dumps('Hello world from github')
   }
